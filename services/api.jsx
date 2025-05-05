01b68b71101afe9d35ce5d056c5bc28bdb749c63
Abdumuhammadjon <nestnext551@gmail.com>
2025-05-05 12:25:15 +0500

    wew

--- services/api.jsx
+++ services/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5001/api"; // Backend server manzili
+const API_URL = "https://backed1.onrender.com/api"; // Backend server manzili
 
 export const getAdmins = async () => {
   const response = await axios.get(`${API_URL}/admins`);
