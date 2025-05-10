1341e477b3c5b6d0b2d43c68a5dc582b8f8ab8a9
Abdumuhammadjon <nestnext551@gmail.com>
2025-05-10 11:24:14 +0500

    Update index.jsx

--- pages/adminlar/index.jsx
+++ pages/adminlar/index.jsx
@@ -78,7 +78,7 @@ export default function Dashboard() {
 
   return (
     <div className="flex -ml-5 flex-col h-screen bg-gray-100">
-  <div className="bg-white shadow-md h-20 sm:h-24 flex items-center px-4 sm:px-6 fixed w-full z-10 top-0">
+  <div className="bg-white shadow-md h-18 sm:h-24 flex items-center px-4 sm:px-6 fixed w-full z-10 top-0">
   <h1 className="text-xl sm:text-2xl font-bold text-gray-800">Dashboard</h1>
 </div>
 
